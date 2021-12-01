464236e680bc90f028250d97debad6f269c4d7ab
Andre gois <andre.s.gois3@gmail.com>
2021-12-01 20:41:53 -0300

    adjusting the create connection

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,26 @@
-import { createConnection } from 'typeorm';
+/*import { createConnection, Connection } from 'typeorm';
 
-(async () => await createConnection())();
+(async () => await createConnection({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "docker",
+  database: "fin_api"
+}))();*/
+
+import { Connection, createConnection, getConnectionOptions } from 'typeorm';
+/*
+
+export default async (host="localhost"): Promise<Connection> => {
+  const defaultOptions = await getConnectionOptions();
+
+  return createConnection(
+    Object.assign(defaultOptions, {
+      host,
+      database: "fin_api",
+    })
+  );
+};*/
+
+createConnection();
